e8fffbae4f94666c9c91147da0ae08e2f993b9b8
Tom Yu <xuejianyuandroid@gmail.com>
2013-05-16 01:20:38 -0500

    Added NoFriction PC

---
+++ Assets/Assets Bursting Brains/Scripts/Player Controllers OVR/_PlayerType.cs
@@ -0,0 +1,14 @@
+/*
+ * IMPORANT:
+ * These enum types must have their names match that of the
+ * player type in the Resources folder.
+ */
+
+
+public enum PlayerType {
+	Ball
+	,Basic
+	,FloatingSphere
+	,NoFriction
+//	,DefaultOculus
+};
