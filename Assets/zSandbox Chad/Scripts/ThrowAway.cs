08cc533f36c5a4252917b6473bf1eff8f10fa206
Chad Custodio <chadcus@gmail.com>
2013-05-23 21:55:26 -0500

    Mouse Movement implemented on camera

---
+++ Assets/zSandbox Chad/Scripts/ThrowAway.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class ThrowAway : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	print ("Hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
