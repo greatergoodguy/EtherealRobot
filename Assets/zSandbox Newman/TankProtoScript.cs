6c473e95e7965214d8614adefce01f92a1a0f514
newmanwillis <newmanwillis@gmail.com>
2013-05-23 19:31:15 -0500

    Newman Sandbox additions

---
+++ Assets/zSandbox Newman/TankProtoScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class TankProtoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)){
+			//transform.position(
+		}	
+		
+		if (Input.GetKey(KeyCode.S)){
+			
+		}
+		
+		if (Input.GetKey(KeyCode.Q)){
+				//YRotation -= rotateInfluence * 0.5f;  
+		}
+		if (Input.GetKey(KeyCode.E)) {
+			//YRotation += rotateInfluence * 0.5f; 
+			
+		}
+	}
+}
