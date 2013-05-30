8d6dcfadc9d6e24fc945bfdaafb4b98d6299fad9
Tom Yu <xuejianyuandroid@gmail.com>
2013-05-29 21:38:58 -0500

    Integrated EtherealPC into main scene

--- Assets/Assets Bursting Brains/Scripts/Player Controllers OVR/EtherealPC.cs
+++ Assets/Assets Bursting Brains/Scripts/Player Controllers OVR/EtherealPC.cs
@@ -3,21 +3,34 @@ using System.Collections;
 
 public class EtherealPC : PlayerController_Deprecated {
 	
-	public float moveSpeed = 6.0f;
-	public float degreePerSecond = 4.0f;
-	public float force = 8.0f;
+	//private float force = 6.0f;
+	//private float moveSpeed = 5.0f;
+	
+	public float turnSensitivity = 2.0f;
+	public float acceleration = 1.0f;
+	public float brakeSpeed = 1.0f;			//dont make larger than max speed
+	public float maxSpeed = 30.0f;
+	public float jumpPower = 5.0f;
+	private float currSpeed = 0.0f;
+	
+	//jump variables
+	private int state = 0;
+	private bool grounded = false;
 	
 	private Vector3 cube;
 	private Vector3 sphere;
 	private Vector3 cubeForward;
 	private Vector3 sphereForward;
 	private Vector3 crossProd;
+	private Vector3 forward;
 	private Vector3 angDirection;
 	private Transform head;
+	private GameObject standardCam;
+	private GameObject oculusCam;
 	
-	private Vector3 forward;
-	protected CameraController_BB 	CameraController 	= null;
-	//protected OVRCameraController 	CameraController 	= null;
+	//Camera Variable
+	//protected CameraController_BB 	CameraController 	= null;
+	protected OVRCameraController 	CameraController 	= null;
 	public float RotationAmount  = 1.5f;
 	private Quaternion OrientationOffset = Quaternion.identity;			// Initial direction of controller (passed down into CameraController)
 	private float 	YRotation 	 = 0.0f;								// Rotation amount from inputs (passed down into CameraController)
@@ -28,10 +41,6 @@ public class EtherealPC : PlayerController_Deprecated {
 	// We should attach objects to this if we want them to rotate 
 	// separately from the head (i.e. the body)
 	protected Transform DirXform = null;
-	
-	
-	private Input_BB activeInput;
-	
 	//
 	// STATIC VARIABLES
 	//
@@ -49,6 +58,7 @@ public class EtherealPC : PlayerController_Deprecated {
 		collider.material.staticFriction2 = .2f;
 		collider.material.frictionCombine = PhysicMaterialCombine.Minimum;
 		
+		/*
 		CameraController_BB[] CameraControllers;
 		CameraControllers = gameObject.GetComponentsInChildren<CameraController_BB>();
 		
@@ -58,7 +68,8 @@ public class EtherealPC : PlayerController_Deprecated {
 			Debug.LogWarning("OVRPlayerController: More then 1 OVRCameraController attached.");
 		else
 			CameraController = CameraControllers[0];
-		/*OVRCameraController[] CameraControllers;
+		*/
+		OVRCameraController[] CameraControllers;
 		CameraControllers = gameObject.GetComponentsInChildren<OVRCameraController>();
 		
 		if(CameraControllers.Length == 0)
@@ -66,26 +77,27 @@ public class EtherealPC : PlayerController_Deprecated {
 		else if (CameraControllers.Length > 1)
 			Debug.LogWarning("OVRPlayerController: More then 1 OVRCameraController attached.");
 		else
-			CameraController = CameraControllers[0];*/
+			CameraController = CameraControllers[0];
 		
 		cube = transform.position;
 		sphereForward = transform.position;
 		head = transform.FindChild("Head");
+		standardCam = head.FindChild("Camera").gameObject;
+		oculusCam = head.FindChild("OVRCameraController").gameObject;
+		
+		standardCam.SetActive(true);
+		oculusCam.SetActive(false);
+		
 		SetCameras();
 		
 		AllowMouseRotation = false;
-		
-		activeInput = GameObject.Find("InputManager").GetComponent<InputManager>().GetActiveInput();
 	}
-	
 	
 	float contAngle = 0;
 	// Update is called once per frame
 	void Update () {
-		//head.transform.rotation = CameraController.transform.rotation;
 		
 		//Gets forward Vector
-		
 		cubeForward = transform.forward;
 		sphereForward = head.forward;
 		
@@ -97,53 +109,64 @@ public class EtherealPC : PlayerController_Deprecated {
 		
 		//Force Vectors
 		Vector3 forwardForce = new Vector3();
+		//Vector3 brakeForce = new Vector3();
 		
-		//Basic Movement
-		//use vector.up for rotating and float degreePerSecond and Time.deltaTime
-		if(activeInput.GetButton_Accel()){
-			//Vector3 tempPos = transform.position;
+		//Basic Movement Acceleration
+		if(Input.GetKey(KeyCode.W)){
+			
 			Vector3 tempAngMove = transform.position;
-			//tempPos += cubeForward * moveSpeed;
-			/*if (angle < 46 && angle > 10 && crossProd.y < 0){
-				degreePerSecond = -80;
+			currSpeed += acceleration;
+			currSpeed = Mathf.Clamp(currSpeed, -maxSpeed, maxSpeed);
+			
+			forwardForce = cubeForward * currSpeed;
+			rigidbody.AddForce(forwardForce);
+		}
+		//Brake Mechanic
+		else{				
+			//brakeForce = -cubeForward * currSpeed;
+			rigidbody.AddForce(rigidbody.velocity * -brakeSpeed);
+		}
+		
+		/*if(Input.GetKey(KeyCode.S)){
+			Vector3 tempPos = transform.position;
+			Vector3 tempAngMove = transform.position;
+			tempPos -= cubeForward * moveSpeed;
+			if (angle < 46 && angle > 10 && crossProd.y < 0){
+				degreePerSecond = 80;
 				tempAngMove = Vector3.up * degreePerSecond * Time.deltaTime;
 				transform.Rotate(tempAngMove);				
 			}
 			else if (angle < 46 && angle > 10 && crossProd.y > 0){
-				degreePerSecond = 80;
+				degreePerSecond = -80;
 				tempAngMove = Vector3.up * degreePerSecond * Time.deltaTime;
 				transform.Rotate(tempAngMove);
-			}*/
-			forwardForce = cubeForward * moveSpeed * force;
-			if (crossProd.y < 0){
-				tempAngMove = GetAngularDirection(absoluteAngle);
-				tempAngMove = -tempAngMove;
-				transform.Rotate(tempAngMove);				
 			}
-			else if (crossProd.y > 0){
-				tempAngMove = GetAngularDirection(absoluteAngle);
-				transform.Rotate(tempAngMove);
-			}
+			tempPos = -cubeForward * moveSpeed * force;
+			rigidbody.AddForce(tempPos);
+		}*/
+		
+		//Steering Mechanics
+		Vector3 angMove = transform.position;
+		if (crossProd.y < 0){
+			angMove = GetAngularDirection(absoluteAngle);
+			angMove = -angMove;
+			transform.Rotate(angMove);				
 		}
-		//if(Input.GetKey(KeyCode.S)){
-			//Vector3 tempPos = transform.position;
-			//Vector3 tempAngMove = transform.position;
-			//tempPos -= cubeForward * moveSpeed;
-			//if (angle < 46 && angle > 10 && crossProd.y < 0){
-				//degreePerSecond = 80;
-				//tempAngMove = Vector3.up * degreePerSecond * Time.deltaTime;
-				//transform.Rotate(tempAngMove);				
-			//}
-			//else if (angle < 46 && angle > 10 && crossProd.y > 0){
-				//degreePerSecond = -80;
-				//tempAngMove = Vector3.up * degreePerSecond * Time.deltaTime;
-				//transform.Rotate(tempAngMove);
-			//}
-			//tempPos = -cubeForward * moveSpeed * force;
-			//rigidbody.AddForce(tempPos);
-		//}
-		angDirection = GetAngularDirection(absoluteAngle);
-		rigidbody.AddForce(forwardForce);
+		else if (crossProd.y > 0){
+			angMove = GetAngularDirection(absoluteAngle);
+			transform.Rotate(angMove);
+		}
+		
+		//Camera Switch
+		if(Input.GetKeyDown(KeyCode.V)){
+			standardCam.SetActive(!standardCam.activeSelf);
+			oculusCam.SetActive(!oculusCam.activeSelf);
+		}
+		
+		//Jump
+		if(grounded && Input.GetKeyDown(KeyCode.LeftShift)){
+			rigidbody.AddForce(Vector3.up * jumpPower * 100);
+		}
 		
 		// Controls the Camera rotation
 		float rotateInfluence = DeltaTime * RotationAmount * RotationScaleMultiplier;
@@ -151,7 +174,7 @@ public class EtherealPC : PlayerController_Deprecated {
 		// Rotate
 		float deltaRotation = 0.0f;
 		if(AllowMouseRotation == false)
-			deltaRotation = activeInput.GetAxis_MouseX() * rotateInfluence * 3.25f;
+			deltaRotation = Input.GetAxis("Mouse X") * rotateInfluence * 3.25f;
 			
 		float filteredDeltaRotation = (sDeltaRotationOld * 0.0f) + (deltaRotation * 1.0f);
 		YRotation += filteredDeltaRotation;
@@ -159,6 +182,7 @@ public class EtherealPC : PlayerController_Deprecated {
 			
 		// Rotate
 		YRotation += OVRGamepadController.GetAxisRightX() * rotateInfluence;
+
 		SetCameras();
 	}
 	
@@ -182,14 +206,30 @@ public class EtherealPC : PlayerController_Deprecated {
 		}
 	}
 	
+	void OnCollisionEnter (){
+		state ++;
+		if(state > 0){
+			grounded = true;
+		}
+	}
+	
+	 
+	void OnCollisionExit (){
+		state --;
+		if(state < 1){
+			grounded = false;
+			state = 0;
+		}
+	}
+	
 	public override string GetControllerName() {
-    	return "Ethereal";
+    	return "Ball";
    	}
 	
 	private Vector3 GetAngularDirection(float angle){
 		Vector3 result = Vector3.zero;
-		angle /= 90;
-		result = Vector3.up * angle * degreePerSecond;
+		angle /= 90;									//scaled for optimal head movement
+		result = Vector3.up * angle * turnSensitivity;
 		return result;
 	}
 	
