fa7fd8c895b77ddb3109657d9cb53f129253e883
newmanwillis <newmanwillis@gmail.com>
2013-05-30 20:23:31 -0500

    Test for Oculus

--- Assets/zSandbox Newman/Scripts/EtherealDualCHGUI.cs
+++ Assets/zSandbox Newman/Scripts/EtherealDualCHGUI.cs
@@ -27,10 +27,29 @@ public class EtherealDualCHGUI : MonoBehaviour {
 	}
 	
 	void OnGUI(){
+		
+		
+		GuiUtilsNW.GUIStereoTexture((int)((centerX + 1 * tickSpacing) - tick1.width/2), (int)(centerY - tick1.height/2), tick1.width, tick1.height, tick1);	
+		GuiUtilsNW.GUIStereoTexture((int)((centerX - 1 * tickSpacing) - tick1.width/2), (int)(centerY - tick1.height/2), tick1.width, tick1.height, tick1);	
+		GuiUtilsNW.GUIStereoTexture((int)((centerX + 2 * tickSpacing) - tick2.width/2), (int)(centerY - tick2.height/2), tick2.width, tick2.height, tick2);
+		GuiUtilsNW.GUIStereoTexture((int)((centerX - 2 * tickSpacing) - tick2.width/2), (int)(centerY - tick2.height/2), tick2.width, tick2.height, tick2);
+		GuiUtilsNW.GUIStereoTexture((int)((centerX + 3 * tickSpacing) - tick3.width/2), (int)(centerY - tick3.height/2), tick3.width, tick3.height, tick3);
+		GuiUtilsNW.GUIStereoTexture((int)((centerX - 3 * tickSpacing) - tick3.width/2), (int)(centerY - tick3.height/2), tick3.width, tick3.height, tick3);
+		/*
+		GuiUtilsNW.GUIStereoTexture((int)(centerX + 1 * tickSpacing), (int)(centerY), tick1.width, tick1.height, tick1);
+		GuiUtilsNW.GUIStereoTexture((int)(centerX - 1 * tickSpacing), (int)(centerY), tick1.width, tick1.height, tick1);
+		GuiUtilsNW.GUIStereoTexture((int)(centerX + 2 * tickSpacing), (int)(centerY), tick2.width, tick2.height, tick2);
+		GuiUtilsNW.GUIStereoTexture((int)(centerX - 2 * tickSpacing), (int)(centerY), tick2.width, tick2.height, tick2);
+		GuiUtilsNW.GUIStereoTexture((int)(centerX + 3 * tickSpacing), (int)(centerY), tick3.width, tick3.height, tick3);
+		GuiUtilsNW.GUIStereoTexture((int)(centerX - 3 * tickSpacing), (int)(centerY), tick3.width, tick3.height, tick3);
+		*/
+		
+		
 		//float cursorX = Screen.width * ((etherealPC.GetAngle() + 180)/360);
 		
 		//DrawTextureCenter(cursorX, centerY, ImageCrosshair);
 		
+		/*
 		DrawTextureCenter(centerX + 1 * tickSpacing, centerY, tick1);
 		DrawTextureCenter(centerX + 2 * tickSpacing, centerY, tick2);
 		DrawTextureCenter(centerX + 3 * tickSpacing, centerY, tick3);
@@ -38,10 +57,10 @@ public class EtherealDualCHGUI : MonoBehaviour {
 		DrawTextureCenter(centerX - 1 * tickSpacing, centerY, tick1);
 		DrawTextureCenter(centerX - 2 * tickSpacing, centerY, tick2);
 		DrawTextureCenter(centerX - 3 * tickSpacing, centerY, tick3);
+		*/
 	}
 	
 	private void DrawTextureCenter(float posX, float posY, Texture texture){
-		GUI.DrawTexture(new Rect(posX/2 - texture.width/2, posY - texture.height/2, texture.width, texture.height), texture);
-		GUI.DrawTexture(new Rect((posX/2 + Screen.width / 2) - texture.width/2, posY - texture.height/2, texture.width, texture.height), texture);
+		GUI.DrawTexture(new Rect(posX - texture.width/2, posY - texture.height/2, texture.width, texture.height), texture);
 	}
 }
