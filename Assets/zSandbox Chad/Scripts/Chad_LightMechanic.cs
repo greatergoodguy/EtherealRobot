8a10008c412be44b81c4ddab20bb335f56065ef1
Oliver Wu <olivergwu@gmail.com>
2013-06-01 19:37:49 -0500

    found a cone editor script, added it, played around with it a little in chad's sandbox, kindofbutnotreallyworks

--- Assets/zSandbox Chad/Scripts/Chad_LightMechanic.cs
+++ Assets/zSandbox Chad/Scripts/Chad_LightMechanic.cs
@@ -20,20 +20,29 @@ public class Chad_LightMechanic : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.FindGameObjectWithTag("Player");
-		spotlight = GameObject.FindGameObjectWithTag("Light");
-		lightCollider = GameObject.FindGameObjectWithTag("Light Boundary");
-		range = spotlight.light.range;
+		//player = GameObject.FindGameObjectWithTag("Player");
+		//spotlight = GameObject.FindGameObjectWithTag("Light");
+		//lightCollider = GameObject.FindGameObjectWithTag("Light Boundary");
+		//range = spotlight.light.range;
 		
 	
 	}
 	
+	/*
+	 * CHAD: I messed around in your sandbox. I didn't really do much, just commented
+	 * out code you weren't using anymore from this script and was testing out using cones
+	 * 
+	 * Found cone editor script here: http://wiki.unity3d.com/index.php?title=CreateCone 
+	 * The script is in our Assets/Editor folder. It probably needs some editing to get 
+	 * it to do light detection more correctly. 
+	 */
+	
 	// Update is called once per frame
 	void Update () {
-		float dist = Vector3.Distance(player.transform.position, spotlight.transform.position);
-		Vector3 rayDirection = new Vector3(spotlight.transform.position.x, 0, spotlight.transform.position.z);
+		//float dist = Vector3.Distance(player.transform.position, spotlight.transform.position);
+		//Vector3 rayDirection = new Vector3(spotlight.transform.position.x, 0, spotlight.transform.position.z);
 		
-		Debug.DrawLine(spotlight.transform.position, hit.point, Color.green);
+		//Debug.DrawLine(spotlight.transform.position, hit.point, Color.green);
 		
 		/*
 		//Point light recognition
@@ -49,7 +58,7 @@ public class Chad_LightMechanic : MonoBehaviour {
 		//Spotlight recognition
 		if(isInLight){
 			print ("Light is good");
-			if(lightIntensity > 2)
+			if(lightIntensity > 1)
 				print ("You must love light");			
 		}
 		else{
