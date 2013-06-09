403f4a5a203f34486df30030abefa47224dda98b
Oliver Wu <olivergwu@gmail.com>
2013-06-09 00:34:27 -0500

    added chad's laser pointer to mech movement prototype

---
+++ Assets/zSandbox Oliver/Scripts/LaserPointer_Chad.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+/* Chad's laser pointer script */
+public class LaserPointer_Chad : MonoBehaviour {
+	
+	private const float LINE_WIDTH = 0.02f;
+	
+	public Color color1 = Color.red;
+	public Color color2 = Color.red;
+	
+	public LineRenderer lineRenderer;
+	
+	void Start () {
+		lineRenderer = (LineRenderer) gameObject.AddComponent("LineRenderer");
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.SetColors(color1, color2);
+		lineRenderer.SetWidth(LINE_WIDTH, LINE_WIDTH);
+		lineRenderer.SetVertexCount(2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 origin = transform.position;
+		Vector3 forwardDirection = transform.forward;
+		
+		//print(forwardDirection);
+		
+		Vector3 endPoint = origin + forwardDirection * 10000;
+		
+		
+		RaycastHit hit = new RaycastHit();
+		
+		lineRenderer.SetPosition(0, origin);
+		
+		if(Physics.Raycast(origin, forwardDirection, out hit)){
+			endPoint = hit.point;
+		}
+		
+		lineRenderer.SetPosition(1, endPoint);
+	
+	}
+}
