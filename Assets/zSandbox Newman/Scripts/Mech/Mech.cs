f92d9af7b9f84730872a012040e1907db141f72e
Newman Willis <newmanwillis@gmail.com>
2013-07-02 16:39:29 -0500

    Mech Grab, both hands

---
+++ Assets/zSandbox Newman/Scripts/Mech/Mech.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+public class Mech : MonoBehaviour {
+	
+	
+	GameObject player;
+	GameObject LeftHand;
+	GameObject RightHand;
+	
+	bool LeftGrab = false;
+	bool RightGrab = false;
+	bool playerIsGrabbed = false;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		LeftHand = GameObject.Find("l_finger_anim");
+		RightHand = GameObject.Find("r_finger_anim");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(!playerIsGrabbed){
+			if(LeftHand.GetComponent<MechLeftHand>().LeftGrabbed){
+				LeftGrab = true;
+				playerIsGrabbed = true;
+				
+			}
+			else if(RightHand.GetComponent<MechRightHand>().RightGrabbed){
+				RightGrab = true;	
+				playerIsGrabbed = true;
+			}
+		}
+		else if(playerIsGrabbed){
+			MechHoldingPlayer();
+		}
+
+	}
+	
+	
+	void MechHoldingPlayer(){
+		Vector3 handPos;
+		if(LeftGrab)
+			handPos = LeftHand.transform.position;
+		else if(RightGrab){
+			handPos = RightHand.transform.position;
+		player.transform.position = handPos;
+		}
+	}
+}
---
+++ Assets/zSandbox Newman/Scripts/Mech/MechRightHand.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class MechRightHand : MonoBehaviour {
+	
+	public bool RightGrabbed = false;	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerStay(Collider other){	
+		if(!RightGrabbed && other.tag.Equals("Player")){
+			RightGrabbed = true;	
+		}
+	}
+}
